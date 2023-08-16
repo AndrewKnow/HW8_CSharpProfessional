ea3aeb8238ca9544364200bd89258e0b271c5beb
Андрей Наумов <NaumovAV1983@yandex.ru>
2023-08-16 22:03:54 +0300

    Добавьте файлы проекта.

---
+++ HW8_CSharpProfessional/Implamentations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW8_CSharpProfessional
+{
+    public class Implamentations
+    {
+        public long AddingArrayNumbers(int[] arr)
+        {
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+
+            sw.Stop();
+
+            return sw.ElapsedMilliseconds;
+        }
+
+
+
+
+    }
+}
---
+++ HW8_CSharpProfessional/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HW8_CSharpProfessional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Implamentations implamentations = new Implamentations();
+
+            var intArray1 = new int[1_00_000];
+            var intArray2 = new int[1_000_000];
+            var intArray3 = new int[10_000_000];
+
+
+            Console.WriteLine("Обычное сложение:");
+            Console.WriteLine($" {implamentations.AddingArrayNumbers(intArray1)}");
+            Console.WriteLine($" {implamentations.AddingArrayNumbers(intArray2)}");
+            Console.WriteLine($" {implamentations.AddingArrayNumbers(intArray3)}");
+
+        }
+    }
+}
